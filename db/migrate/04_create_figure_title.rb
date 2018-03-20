4bd007bfe3d85a6c86aeb6004262ffaac19f57cd
David Kuruvilla <dapawn@gmail.com>
2018-03-20 18:24:48 +0000

    Done.

--- db/migrate/04_create_figure_title.rb
+++ db/migrate/04_create_figure_title.rb
@@ -1,6 +1,6 @@
-class CreateFigureTitle < ActiveRecord::Migration[4.2]
+class CreateFigureTitles < ActiveRecord::Migration[4.2]
   def change
-    create_table :figure_title do |t|
+    create_table :figure_titles do |t|
       t.integer :figure_id
       t.integer :title_id
     end
